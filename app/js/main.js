6e110c2c2b1c674e168de7d73d078d296d26aecf
Mikhail <mikhail.sapuzhak@gmail.com>
2020-03-01 22:41:56 +0200

    header

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,18 @@
-$(function(){
+$(function () {
+
+  $('.header__slider').slick({
+    infinite: true,
+    fade: true,
+    prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrow-left.svg" alt="arrow"></img>',
+    nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrow-right.svg" alt="arrow"></img>',
+    asNavFor: '.slider-dots',
+  });
+
+  $('.slider-dots').slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    asNavFor: '.header__slider',
+  });
 
 
-
-  
 });
