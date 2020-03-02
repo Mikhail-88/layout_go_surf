410b861b129879623fe40e898daa3d5efd2c6e1a
Mikhail <mikhail.sapuzhak@gmail.com>
2020-03-02 20:38:21 +0200

    add two new blocks

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # layout_go_surf
+
+- [DEMO-LINK](https://mikhail-88.github.io/layout_go_surf/app/index.html)
--- app/js/main.js
+++ app/js/main.js
@@ -5,13 +5,29 @@ $(function () {
     fade: true,
     prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrow-left.svg" alt="arrow"></img>',
     nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrow-right.svg" alt="arrow"></img>',
-    asNavFor: '.slider-dots',
+    asNavFor: '.slider-line',
   });
 
-  $('.slider-dots').slick({
+  $('.slider-line').slick({
     slidesToShow: 4,
     slidesToScroll: 4,
     asNavFor: '.header__slider',
+  });
+
+  $('.surf-slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrow-left.svg" alt="arrow"></img>',
+    nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrow-right.svg" alt="arrow"></img>',
+    asNavFor: '.slider-map',
+  });
+
+  $('.slider-map').slick({
+    slidesToShow: 8,
+    slidesToScroll: 1,
+    arrows: false,
+    asNavFor: '.surf-slider',
+    focusOnSelect: true,
   });
 
 
